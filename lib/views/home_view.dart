a28d00cf401899618bbf9f8d5cc51e1d3ca52aad
viktorzaharov <viktorzahar@gmail.com>
2022-02-23 11:29:17 -0500

    fix home_view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:simple_dart_web_widgets/labels/simple_label.dart';
 
 class HomeView extends View {
   HomeView() : super('HomeView') {
+    id='home';
     caption = 'Home';
     vertical = true;
     padding = '20px';
